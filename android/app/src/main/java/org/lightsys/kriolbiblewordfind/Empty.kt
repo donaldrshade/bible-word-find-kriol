747bd75a65aa7c7c656fef2493bce532c733f48f
donaldrshade <donaldrshade@cedarville.edu>
2020-03-03 16:36:36 -0700

    Updated some files

--- android/app/src/main/java/org/lightsys/kriolbiblewordfind/Empty.kt
+++ android/app/src/main/java/org/lightsys/kriolbiblewordfind/Empty.kt
@@ -11,12 +11,6 @@ class Empty : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_empty)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
 }
