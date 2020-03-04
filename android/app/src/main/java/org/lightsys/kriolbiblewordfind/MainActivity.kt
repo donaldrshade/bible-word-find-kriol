a224a1bf4a9c6471ed29e20bce581d60a424fe36
Gaden32 <48032360+Gaden32@users.noreply.github.com>
2020-03-03 17:37:35 -0700

    Removed titalbar

--- android/app/src/main/java/org/lightsys/kriolbiblewordfind/MainActivity.kt
+++ android/app/src/main/java/org/lightsys/kriolbiblewordfind/MainActivity.kt
@@ -5,13 +5,22 @@ import android.os.Bundle
 import android.support.constraint.ConstraintLayout
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Set Activity to full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         setContentView(R.layout.activity_main)
+
 
         val fab = findViewById<FloatingActionButton>(R.id.home_fab)
         fab.setOnClickListener { view ->
