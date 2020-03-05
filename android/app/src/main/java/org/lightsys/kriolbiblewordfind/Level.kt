285bfef1097f4bd6a2e8cd44d1cdcf9a041fa6bc
donaldrshade <donaldrshade@cedarville.edu>
2020-03-05 12:34:17 -0700

    Added New Files
    Added the new files need to reset the DB code to show new names

--- android/app/src/main/java/org/lightsys/kriolbiblewordfind/Level.kt
+++ android/app/src/main/java/org/lightsys/kriolbiblewordfind/Level.kt
@@ -1,5 +1,19 @@
 package org.lightsys.kriolbiblewordfind
 
+import android.content.Context
+import java.io.File
+import java.io.FileOutputStream
+
 class Level(var id:Int = -1,var title:String = "",var completed:Boolean = false,var picture:String = "",var word_file:String = ""){
+    fun getWordList(packageName: String, context: Context): ArrayList<String> {
+        val resID = context.resources.getIdentifier(word_file, "drawable", packageName);
+        val wordInputStream = context.resources.openRawResource(resID);
+        var words:ArrayList<String> = ArrayList()
+        wordInputStream.bufferedReader().useLines { lines -> lines.forEach{
+                words.add(it.trim())
+            }
+        }
+        return words
+    }
 
 }
--- android/app/src/main/java/org/lightsys/kriolbiblewordfind/Puzzle.kt
+++ android/app/src/main/java/org/lightsys/kriolbiblewordfind/Puzzle.kt
@@ -1,5 +1,15 @@
 package org.lightsys.kriolbiblewordfind
 
-class Puzzle(var id:Int = -1,var size:Int = -1,var numOfWords:Int = -1,var completed:Boolean=false,var audioFile:String="",var level_id:Int=-1,var min_num_letters:Int=-1,var max_num_letters:Int=-1 ) {
+import android.content.Context
+import android.content.res.Resources
 
+class Puzzle(var id:Int = -1,var size:Int = -1,var numOfWords:Int = -1,var completed:Boolean=false,var audioFile:String="",var level_id:Int=-1,var min_num_letters:Int=-1,var max_num_letters:Int=-1 ) {
+    fun getWordList(packageName: String, context: Context): ArrayList<String> {
+        val db = Database(context)
+        val level = db.getLevel(level_id)
+        return level.getWordList(packageName, context)
+    }
+    fun getAudioFileId(packageName: String, context: Context): Int {
+        return context.resources.getIdentifier(audioFile, "drawable", packageName);
+    }
 }
