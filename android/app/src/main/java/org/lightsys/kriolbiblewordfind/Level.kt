f98230d9278f595fee5f215fc6bda10eb8b61894
donaldrshade <donaldrshade@cedarville.edu>
2020-03-05 15:49:16 -0700

    Updated Database and linked to external files
    This is valid and the puzzles have been inserted...

--- android/app/src/main/java/org/lightsys/kriolbiblewordfind/Level.kt
+++ android/app/src/main/java/org/lightsys/kriolbiblewordfind/Level.kt
@@ -5,15 +5,18 @@ import java.io.File
 import java.io.FileOutputStream
 
 class Level(var id:Int = -1,var title:String = "",var completed:Boolean = false,var picture:String = "",var word_file:String = ""){
-    fun getWordList(packageName: String, context: Context): ArrayList<String> {
-        val resID = context.resources.getIdentifier(word_file, "drawable", packageName);
-        val wordInputStream = context.resources.openRawResource(resID);
+    fun getWordList(context: Context): ArrayList<String> {
+        val resID = context.resources.getIdentifier(word_file, "raw", context.getString(R.string.package_name));
+        val wordInputStream = context.resources.openRawResource(resID)
         var words:ArrayList<String> = ArrayList()
         wordInputStream.bufferedReader().useLines { lines -> lines.forEach{
                 words.add(it.trim())
             }
         }
         return words
+    }
+    fun getImageFileId(context: Context): Int {
+        return context.resources.getIdentifier(picture, "raw", context.getString(R.string.package_name));
     }
 
 }
