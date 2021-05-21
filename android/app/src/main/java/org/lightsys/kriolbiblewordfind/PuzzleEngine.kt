aff624621645dcbfbd1f5ac3ec9fe0e52adb7b58
Ben Smid <bojsmid@gmail.com>
2021-05-21 17:04:39 -0400

    remove min word length from PuzzleEngine.kt when selecting words for the options array from pool

--- android/app/src/main/java/org/lightsys/kriolbiblewordfind/PuzzleEngine.kt
+++ android/app/src/main/java/org/lightsys/kriolbiblewordfind/PuzzleEngine.kt
@@ -300,7 +300,7 @@ class PuzzleEngine(var puzzle: Puzzle, var context: Context) {
             var wordSizes = puzzle.getWordSizes()
             val options = ArrayList<String>()
             for (i in pool.indices) {
-                if (pool[i].length >= wordSizes[0] && pool[i].length <= wordSizes[1]) {
+                if (/* pool[i].length >= wordSizes[0] && */ pool[i].length <= wordSizes[1]) {
                     options.add(pool[i])
                 }
             }
